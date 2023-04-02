77123c5eadc8e0954c88e7ad362ce71e3201d35a
yeonghoon Park <huklmnj2@naver.com>
2023-04-02 23:51:02 +0900

    Revert "[components] AppPagination3.jsx / create HTML & add functions"
    This reverts commit ab09dce18743995ce7e644085b7ba38d19a04a24.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./layouts/Home";
 import Home2 from "./layouts/Home2";
-import Home3 from "./layouts/Home3";
 
 function App() {
   return (
@@ -10,8 +9,7 @@ function App() {
       <HashRouter>
         <Routes>
           {/* <Route path='/' element={<Home />} /> */}
-          {/* <Route path='/' element={<Home2 />} /> */}
-          <Route path='/' element={<Home3 />} />
+          <Route path='/' element={<Home2 />} />
         </Routes>
       </HashRouter>
     </div>
